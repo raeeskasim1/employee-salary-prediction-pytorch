ace727a47e51addbfa81d8a02fe62563ae3b9ae7
raeeskasim1 <raeeskasim84@gmail.com>
2026-07-24 20:26:02 +0530

    Create EmployeeClassifier model architecture

---
+++ model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class EmployeeClassifier(nn.Module):
+    def __init__(self,input_size):
+        super().__init__()
+
+        self.network=nn.Sequential(
+            nn.Linear(input_size,16),
+            nn.ReLU(),
+
+            nn.Linear(16,8),
+            nn.ReLU(),
+
+            nn.Linear(8,1),
+            nn.Sigmoid()
+        )
+
+    def forward(self,x):
+        return self.network(x)
